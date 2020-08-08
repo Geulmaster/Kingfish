40dff4fcfd34a42f3d2077ace2f4a4d95e2075d5
Geulmaster <eyal.geulayev@gmail.com>
2020-08-08 23:24:47 +0300

    Few publish changes

---
+++ __init__.py
@@ -0,0 +1,2 @@
+from Core import logger
+from DBs import connect_to_hosts, mongodb_wizard, sql_wizard
--- setup.py
+++ setup.py
@@ -1,22 +1,28 @@
+from pathlib import Path
 import setuptools
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
+HERE = Path(__file__).parent
+README = (HERE / "README.md").read_text()
 
 setuptools.setup(
     name="Kingfish",
-    version="0.0.1",
-    author="Eyal Geulayev",
-    author_email="eyal.geulayev@gmail.com",
+    version="0.0.7",
     description="My first published package",
-    long_description=long_description,
+    long_description=README,
     long_description_content_type="text/markdown",
     url="https://github.com/Geulmaster/Kingfish",
-    packages=setuptools.find_packages(),
+    author="Eyal Geulayev",
+    author_email="eyal.geulayev@gmail.com",
+    license="MIT",
     classifiers=[
-        "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
     ],
+    packages=["Core", "DBs", "Special", "Wrapper"],
+    include_package_data=True,
+    install_requires=["pymongo", "paramiko", "psycopg2-binary"],
     python_requires='>=3.6',
+    entry_points={
+    },
 )
