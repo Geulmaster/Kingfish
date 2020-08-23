b71875feb2a48df502b4a59de1c14392998974ca
Geulmaster <eyal.geulayev@gmail.com>
2020-08-23 23:29:30 +0300

    Add mongodb connections test

--- Tests/test_mongodb.py
+++ Tests/test_mongodb.py
@@ -1,5 +1,8 @@
 from Kingfish.DBs import mongodb_wizard
 
+def test_connections():
+    mongodb_wizard.connect_to_mongodb()
+
 def test_insertion_to_db():
     mongodb_wizard.insert_to_mongodb("users", data = "dodo", dsdsd = "dsds")
     mongodb_wizard.find_documents(value = "dodo", key = "data")
