6b005b42fe27edd3f34214b0960f9ea329e2e4a7
Geulmaster <eyal.geulayev@gmail.com>
2020-08-24 00:43:00 +0300

    Add document validation test

--- Tests/test_mongodb.py
+++ Tests/test_mongodb.py
@@ -3,6 +3,9 @@ from Kingfish.DBs import mongodb_wizard
 def test_connections():
     mongodb_wizard.connect_to_mongodb()
 
+def test_document_creation():
+    assert type(mongodb_wizard.create_document(pop = "lool", word = "word")) is dict
+
 def test_insertion_to_db():
     mongodb_wizard.insert_to_mongodb("users", data = "dodo", dsdsd = "dsds")
     mongodb_wizard.find_documents(value = "dodo", key = "data")
