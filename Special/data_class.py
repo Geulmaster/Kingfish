7e15b68e8e8812ce91fecc4df967514e5ff821c2
Geulmaster <eyal.geulayev@gmail.com>
2021-03-28 23:03:49 +0300

    Add another instance of namedtuple

--- Special/data_class.py
+++ Special/data_class.py
@@ -23,8 +23,12 @@ from collections import namedtuple
 Point = namedtuple('Point', ["x", "y"])
 p = Point(11, 22)
 
+Person = namedtuple('Person', ["name", "age", "hobby"])
+p2 = Person("Serjio", "40", "Video Games")
 """
 Examples:
 p.x + p.y = 33
 p[0] + p[1] = 33
+
+print(f"hello {p2.name}")
 """
